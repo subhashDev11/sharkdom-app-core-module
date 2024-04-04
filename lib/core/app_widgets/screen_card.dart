ac568f7b4cfdf0f106077594136ffa469109711d
Subhash Chandra Shukla <subhashdev011@gmail.com>
2024-04-04 20:44:02 +0530

    Update screen_card.dart

--- lib/core/app_widgets/screen_card.dart
+++ lib/core/app_widgets/screen_card.dart
@@ -4,10 +4,12 @@ import 'package:app_core_module/core/app_extension.dart';
 import '../color_constant.dart';
 
 class ScreenCard extends StatelessWidget {
-  const ScreenCard({super.key, required this.child, this.showIndicator});
+  const ScreenCard({super.key, required this.child, this.showIndicator,
+    this.elevation=0,});
 
   final bool? showIndicator;
   final Widget child;
+  final int elevation;
 
   @override
   Widget build(BuildContext context) {
@@ -18,39 +20,40 @@ class ScreenCard extends StatelessWidget {
         }
       },
       child: Card(
+        elevation: elevation,
         margin: EdgeInsets.zero,
         shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(25),
-          topRight: Radius.circular(25),
-        )),
+              topLeft: Radius.circular(25),
+              topRight: Radius.circular(25),
+            )),
         color: Theme.of(context).brightness == Brightness.light ? Colors.white : null,
         child: (showIndicator ?? true)
             ? Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const SizedBox(
-                    height: 20,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Container(
+                height: 7.h,
+                width: context.screenSize.width * 0.3,
+                decoration: ShapeDecoration(
+                  color: ColorConstant.divideColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24242),
                   ),
-                  Center(
-                    child: Container(
-                      height: 7.h,
-                      width: context.screenSize.width * 0.3,
-                      decoration: ShapeDecoration(
-                        color: ColorConstant.divideColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(24242),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  child,
-                ],
-              )
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            child,
+          ],
+        )
             : child,
       ),
     );
